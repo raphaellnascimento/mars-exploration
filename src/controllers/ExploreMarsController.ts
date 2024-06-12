6720319f026a12b23d480972e44246a11618d2f3
raphael.nascimento <raphael.nascimento@article.com>
2024-06-12 20:26:07 -0300

    es lint addition

--- src/controllers/ExploreMarsController.ts
+++ src/controllers/ExploreMarsController.ts
@@ -1,9 +1,8 @@
 import { RequestHandler,  } from 'express';
 import MarsRoverService from "../services/MarsRoverService";
 
-export const exploreMars:RequestHandler = (req, res, next) => {
+export const exploreMars:RequestHandler = (req, res) => {
     const input:string = req.body;
-
     if (!input) {
         return res.status(400).send('Instructions data is required');
     }
